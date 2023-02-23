35873696fb76ffb29c20d64ba10c25742793f9ea
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-02-23 14:21:40 +1300

    added answer for question 1

---
+++ Labs/lab-1-1-1-Q1.py
@@ -0,0 +1,14 @@
+# Lab 1-1-1 Question 1 
+# Flynn Stevens
+# fas0265@arastudent.ac.nz
+
+# Print the following message:
+# Hi.
+# Welcome to the programming course
+# BCDE101
+
+print("""Hi.
+Welcome to the programming course
+BCDE101""")
+
+# I used the triple quotes to print the message, this is called a multi-line string.
