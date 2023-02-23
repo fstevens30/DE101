17e56f761d28622db5b28e2dbcd527cfe096123d
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-02-23 14:28:12 +1300

    added answer for question 2

---
+++ Labs/lab-1-1-1-Q2.py
@@ -0,0 +1,11 @@
+# Lab 1-1-1 Question 2
+# Flynn Stevens
+# fas0265@arastudent.ac.nz
+
+# Ask the user for their name, then give a personalised greeting.
+
+name = input('Please enter your first name: ')
+
+print('Hi')
+print(name)
+print('Pleased to meet you.')
