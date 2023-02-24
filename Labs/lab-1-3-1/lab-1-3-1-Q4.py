7ee6729aed20c36d7f5f3d478ea0029ae73921fc
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-02-24 23:46:28 +1300

    Started Q4

---
+++ Labs/lab-1-3-1/lab-1-3-1-Q4.py
@@ -0,0 +1,18 @@
+# Lab 1-3-1 Q1
+# Flynn Stevens
+# fas0265@arastudent.ac.nz
+
+# Write a currency exchange application.
+
+# Ask the user to enter their name, the current NZ to AU exchange rate 
+# and how much NZ money they want exchange and then display the equivalent in Australian dollars.
+
+# get the users name
+first_name = input('Please enter your first name: ')
+
+# get the exchange rate
+exchange_rate = float(input(f'Hi {first_name}. Please enter the NZ/AU exchange rate: '))
+
+# get the amount of NZ dollars to exchange
+nz_dollars = float(input('Please enter the amount of NZ $`s you want to exchange: '))
+
