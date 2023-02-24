93d8659990fed3368d18fdcb3387f37c0e9d1820
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-02-24 22:27:42 +1300

    Uploaded latest work and restructured folders

---
+++ Labs/lab-1-3-1/lab-1-3-1-Q1.py
@@ -0,0 +1,13 @@
+# Lab 1-2-1 Q1
+# Flynn Stevens
+# fas0265@arastudent.ac.nz
+
+# Numbers
+
+#Write a program which asks the user to enter an integer and using a standard increment statement,
+#increment the integer by one and display the message.
+
+num = input('Please enter an integer: ')
+num = int(num)
+num_plus_one = num + 1
+print(f'The answer to {num} + 1 is {num_plus_one}')
