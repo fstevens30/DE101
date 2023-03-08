646ccb691cb0e79dc7ad5bad60bbebe3d2936375
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-03-08 14:46:39 +1300

    Fixed indexes for better compatability

--- Labs/lab-2-2-1/lab-2-2-1-Q6.py
+++ Labs/lab-2-2-1/lab-2-2-1-Q6.py
@@ -15,7 +15,7 @@ is_sun = input('Is it Sunday? (y/n): ').strip().lower()
 is_low_tide = input('Is the tide out? (y/n): ').strip().lower()
 
 #Answer
-final_answer = str(should_we_go[0] == 'y' and (is_sat == 'y' and is_sun != 'y' or is_sun == 'y' and is_sat != 'y') and is_low_tide[0] == 'y').lower()
+final_answer = str(should_we_go[0] == 'y' and (is_sat[0] == 'y' and is_sun[0] != 'y' or is_sun[0] == 'y' and is_sat[0] != 'y') and is_low_tide[0] == 'y').lower()
 
 # Print the results
 print()
