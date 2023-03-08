78dcd7938ce070f46ab2d4906d4c6367ee12b14b
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-03-08 14:31:48 +1300

    Added notes for week 3 lesson 1

---
+++ Notes/wk3-L1.md
@@ -0,0 +1,21 @@
+# Psuedocode Examples
+
+- Get coffee, kettle, water, milk, sugar, cup and a spoon
+
+Put water in kettle
+Boil kettle
+Put coffee into cup
+
+Ask the user if they want milk
+If user wants milk
+	Add milk to cup
+End if
+
+Ask the user if they want sugar
+If user wants sugar
+	Add sugar to cup
+End if
+
+Fill cup to within 10mm of the top with water from kettle
+Stir with spoon
+Serve coffee
---
+++ Notes/wk3-L1.py
@@ -0,0 +1,29 @@
+# Week 3 Lesson 1
+
+# Introduction to Programming
+# Conditional statements 1
+
+# Psuedocode
+# Psuedocode is a way of writing code without using the syntax of a programming language to describe the logic of a program.
+# Examples of psuedocode can be found in wk3.L1.md
+
+# Contitional statements
+# Example
+
+first_number = 1
+second_number = 2
+
+if first_number > second_number:
+    print('The first number is greater than the second number.')
+
+# Returns nothing because the first number is not greater than the second number.
+
+# Example 2
+
+another_first_number = 3
+another_second_number = 2
+
+if another_first_number > another_second_number:
+    print('The first number is greater than the second number.')
+
+# Returns 'The first number is greater than the second number.' because the first number is greater than the second number.
