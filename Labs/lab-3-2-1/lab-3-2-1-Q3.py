8591cc429fe38eba16dc3f65f0166c0dc8c2ea89
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-03-09 14:22:54 +1300

    Added psuedocode for Q3 to continue

---
+++ Labs/lab-3-2-1/lab-3-2-1-Q3.py
@@ -0,0 +1,16 @@
+# Lab 3-2-1 Question 3
+# Flynn Stevens
+# fas0265@arastudent.ac.nz
+
+# Start 
+print('Integer comparator.')
+
+# Get first name
+
+# Get first num
+
+# Get second num
+
+# Get third num
+
+# Logic
