8a5df037f2c1d98eb036eb5d1e02b4d31b2c8037
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-03-10 13:59:23 +1300

    Refactored

--- Labs/lab-3-2-1/lab-3-2-1-Q2.py
+++ Labs/lab-3-2-1/lab-3-2-1-Q2.py
@@ -14,3 +14,16 @@ if age % 2 == 0: # We can use the modulus operator to check if the number is eve
     print(f'{name}, {age} is an even number of years.')
 else:
     print(f'{name}, {age} is an odd age.')
+
+# Refactor
+
+refname = input('Please enter your first name: ').strip().title()
+refage = int(input(f'Hi {refname}, please enter your age in years: ').strip())
+
+TRUE = print(f'{name}, {age} is an even number of years.')
+FALSE = print(f'{name}, {age} is an odd age.')
+
+if age % 2 == 0:
+    TRUE
+else:
+    FALSE
